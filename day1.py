12e0b62ee7f2da8bff3659d2c21150c8ccd97634
Daniel Brynza <danielbrynza@gmail.com>
2026-09-06 21:48:19 +0300

    Test save 3

--- day1.py
+++ day1.py
@@ -1,6 +1,13 @@
-number=int(input("Enter a number 1-10:"))
-for x in range(1,number+1):
-    if x%3==0:
-        print(x)
-    else:
-        print("Git practice")
+number=int(input("Write number 1-5\n"))
+if number == 5:
+    print("Very good")
+elif number == 4:
+    print("Good")
+elif number == 3:
+    print("Normal")
+elif number == 2:
+    print("Bad")
+elif number == 1:
+    print("Very bad")    
+else:
+    print("Unncorrect number")
