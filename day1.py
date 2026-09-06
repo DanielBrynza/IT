f8e4bf56a20745d4bdcd9241507079b524c35c2c
Daniel Brynza <danielbrynza@gmail.com>
2026-09-06 21:56:22 +0300

    Test save 4

--- day1.py
+++ day1.py
@@ -1,13 +1,5 @@
-number=int(input("Write number 1-5\n"))
-if number == 5:
-    print("Very good")
-elif number == 4:
-    print("Good")
-elif number == 3:
-    print("Normal")
-elif number == 2:
-    print("Bad")
-elif number == 1:
-    print("Very bad")    
+day=input("Введите день недели:\n")
+if not day=="Суббота" or day== "Воскресение":
+    print("Выходной")
 else:
-    print("Unncorrect number")
+    print("Рабочий")
